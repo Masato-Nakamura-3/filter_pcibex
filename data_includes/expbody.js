a66dbeba446a06871aea49f3eeb98f3810962673
Masato-Nakamura-3 <mnaka@umd.edu>
2021-03-12 12:21:04 -0500

    test an if version

--- data_includes/expbody.js
+++ data_includes/expbody.js
@@ -186,7 +186,24 @@ Template(
             .start()
             .wait()
         ,
+        if (row.context_n > 8) {
 
+        newText("w9", row.w9)
+            .css({"font-size":"40"})
+            .print("center at 50vw", "middle at 40vh")
+        ,
+        newTimer(300)
+            .start()
+            .wait()
+        ,
+        getText("w9")
+            .remove()
+        ,
+        newTimer(230)
+            .start()
+            .wait()
+        }
+        ,
 //        newMediaRecorder(row.filename+"_"+subject_id,"audio")
 //            .record()
 //        ,
